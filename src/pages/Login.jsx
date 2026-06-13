fd8f026c1cf0f06bd120fd0d522dc72c6ac96e0d
mahmoud mraiwa <151856858+MAHMRA001@users.noreply.github.com>
2026-06-13 02:47:18 +0200

    Fix login redirect to welcome

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Login() {
   useEffect(() => {
-    window.location.href = "/";
+    window.location.replace("/welcome");
   }, []);
 
   return (
